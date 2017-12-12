313081d8cef5a9a00fd624431b3d0f392467ba88
airende <zhuye@myweimai.com>
2017-12-12 11:25:03 +0800

    添加注释

--- runtime/runtime/NSURL+ARDURL.m
+++ runtime/runtime/NSURL+ARDURL.m
@@ -13,20 +13,23 @@
 
 +(void)load{
 
+    //获取一个类的方法
     Method oldMethod = class_getClassMethod([NSURL class], @selector(URLWithString:));
     Method newMethod = class_getClassMethod([NSURL class], @selector(ARD_URLWithString:));
     
+    //交换两个类的方法实现
     method_exchangeImplementations(oldMethod, newMethod);
     
 }
 
-
+//替换系统方法的自写方法
 + (instancetype)ARD_URLWithString:(NSString *)URLString{
     
     if ([URLString isEqualToString:@"www.baidu.com"]) {
         NSLog(@"你输的是百度网址");
 //        return nil;
     }
+    //这个方法的实现已经变成了系统的方法
     return [self ARD_URLWithString:URLString];
 }
 
